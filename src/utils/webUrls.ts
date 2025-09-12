b49985ed46aa7dd3722013b0d536cfbd7fbf2d1a
shenyu <wangshenyu@baidu.com>
2025-09-12 14:59:30 +0800

    社区展示

--- src/utils/webUrls.ts
+++ src/utils/webUrls.ts
@@ -1,17 +1,31 @@
 /**
- * 根据攻略ID获取对应的web展示URL
+   * 根据攻略ID获取对应的web展示URL
  * 只有特定的攻略才有对应的外部展示页面
  */
 export const getWebUrlByGuideId = (guideId: string, isUpgraded: boolean = false): string => {
   switch (guideId) {
     case 'uk-harry-potter-7days':
-      return !isUpgraded 
-        ? 'https://v0-harry-potter-trip-zf.vercel.app/'
-        : `${process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3030'}/templates/uk-harry-potter-7days.html`
+      return `${process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3030'}/templates/templates_uk_7days.html`
     case 'japan-sakura-7days':
-      return !isUpgraded 
-        ? 'https://v0-japan-travel-website-six.vercel.app/'
-        : `${process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3030'}/templates/japan-7days.html`
+      return `${process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3030'}/templates/templates_jp_7days.html`
+    case 'chengdu-3days':
+      return `${process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3030'}/templates/templates_chengdu_3days.html`
+    case 'chongqing-5days':
+      return `${process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3030'}/templates/templates_chongqing_5days.html`
+    case 'datong-3days':
+      return `${process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3030'}/templates/templates_datong_3days.html`
+    case 'japan-3days':
+      return `${process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3030'}/templates/templates_jp_3days.html`
+    case 'lasa-7days':
+      return `${process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3030'}/templates/templates_lasa_7days.html`
+    case 'sanya-3days':
+      return `${process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3030'}/templates/templates_sanya_3days.html`
+    case 'usa-7days':
+      return `${process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3030'}/templates/templates_usa_7days.html`
+    case 'xiamen-3days':
+      return `${process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3030'}/templates/templates_xiamen_3days.html`
+    case 'xizang-7days':
+      return `${process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3030'}/templates/templates_xizang_7days.html`
     default:
       return `${process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3030'}/shared/${guideId}.html` // 其他攻略暂无对应的web展示页面
   }
